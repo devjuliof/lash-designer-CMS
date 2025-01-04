e749b0f36f452bb006e1ab69ab5f627304c65070
julio <juliofariadev@gmail.com>
2025-01-04 17:30:09 -0300

    fix(PinkButton): correct onClick type mismatch

--- frontend/src/app/shared/components/PinkButton.tsx
+++ frontend/src/app/shared/components/PinkButton.tsx
@@ -1,8 +1,8 @@
-type InputProps = {
+type ButtonProps = {
   text: string;
-  onClick: React.MouseEventHandler<HTMLInputElement>;
+  onClick: React.MouseEventHandler<HTMLButtonElement>;
 }
 
-export default function PinkButton({text, onClick}: InputProps) {
+export default function PinkButton({text, onClick}: ButtonProps) {
   return <button className="bg-pink-600 text-white px-12 py-3 rounded-md font-bold" onClick={onClick}>{text}</button>
 }
