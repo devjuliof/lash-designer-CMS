ca77f756c457b86e058ade07913ebaf4f5687600
julio <juliofariadev@gmail.com>
2025-01-07 21:37:18 -0300

    feat(clientService): Add new method to create AnamnesisForm

--- backend/src/clients/providers/clients.service.ts
+++ backend/src/clients/providers/clients.service.ts
@@ -7,12 +7,17 @@ import { CreateClientDto } from '../dtos/create-client.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Client } from '../entities/client.entity';
 import { Repository } from 'typeorm';
+import { CreateAnamnesisFormDto } from '../dtos/create-anamnesis-form.dto';
+import { AnamnesisForm } from '../entities/anamnesis-form.entity';
 
 @Injectable()
 export class ClientService {
   constructor(
     @InjectRepository(Client)
     private readonly clientRepository: Repository<Client>,
+
+    @InjectRepository(AnamnesisForm)
+    private readonly anamnesisFormRepository: Repository<AnamnesisForm>,
   ) {}
 
   public async createClient(createClientDto: CreateClientDto) {
@@ -54,5 +59,28 @@ export class ClientService {
     }
 
     return { deleted: true };
+  }
+
+  public async createAnamnesisForm(
+    createAnamnesisFormDto: CreateAnamnesisFormDto,
+  ) {
+    // You need handle with exceptions
+    const client = await this.clientRepository.findOneBy({
+      id: createAnamnesisFormDto.clientId,
+    });
+
+    const newAnamnsesisForm = this.anamnesisFormRepository.create(
+      createAnamnesisFormDto,
+    );
+
+    client.anamnesisForm = newAnamnsesisForm;
+
+    await this.anamnesisFormRepository.save({
+      ...newAnamnsesisForm,
+    });
+
+    await this.clientRepository.save(client);
+
+    return newAnamnsesisForm;
   }
 }
