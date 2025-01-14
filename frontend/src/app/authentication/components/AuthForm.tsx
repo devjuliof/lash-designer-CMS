dce179c649b320486f40d1c53db7576bd157426d
julio <juliofariadev@gmail.com>
2025-01-14 11:23:29 -0300

    feat: replace HTML code with reusable component

--- frontend/src/app/authentication/components/AuthForm.tsx
+++ frontend/src/app/authentication/components/AuthForm.tsx
@@ -2,9 +2,9 @@
 
 import React from 'react';
 import PinkButton from '../../shared/components/PinkButton';
-import InputWithLabel from './InputWithLabel'
 import { AuthService } from '../services/authService';
 import { redirect } from "next/navigation";
+import InputWithLabel from '@/app/shared/components/InputWithLabel';
 
 export default function AuthForm() {
   const [accessCode, setAccessCode] = React.useState<string>('');
@@ -30,7 +30,7 @@ export default function AuthForm() {
     <div className="bg-white flex flex-col gap-10 h-auto w-full mx-4 p-4 rounded-xl">
       <h1 className='text-2xl text-center font-bold'>Entrar</h1>
       <div>
-        <InputWithLabel inputOnChange={inputOnChange}/>
+        <InputWithLabel label={'Digite seu código de acesso'} placeholder={'Código'} onChange={inputOnChange} />
         {err && <p className='text-red-600'>{err}</p>}
       </div>
       <PinkButton text={'Entrar'} onClick={handleFormSubmit}/>
