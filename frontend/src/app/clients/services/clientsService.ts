cb8640c1832d369bd4a0b752d98366b379c2ca72
julio <juliofariadev@gmail.com>
2025-01-14 11:29:12 -0300

    feat: add register client method

--- frontend/src/app/clients/services/clientsService.ts
+++ frontend/src/app/clients/services/clientsService.ts
@@ -1,3 +1,5 @@
+import { ClientData } from "../interfaces/client-data.interface";
+
 export class ClientsService {
   public static async getAllClients() {
     const results = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/clients/?limit=10&page=1`).then(res => res.json());
@@ -9,5 +11,17 @@ export class ClientsService {
     const results = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/clients/search-clients/?name=${inputText}`).then(res => res.json());
 
     return results;
+  }
+
+  public static async registerClient(clientData: ClientData) {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/clients`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(clientData),
+    }).then(res => res.json());
+
+    return response;
   }
 }
