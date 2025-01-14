0a250d54c42c42ed85f7fa16f3cd7e9d01c3aa3b
julio <juliofariadev@gmail.com>
2025-01-14 11:52:10 -0300

    feat: add function props

--- frontend/src/app/clients/components/ClientProfile.tsx
+++ frontend/src/app/clients/components/ClientProfile.tsx
@@ -5,16 +5,19 @@ import trashIcon from '../assets/trash-icon.svg';
 
 interface ClientProfileProps {
   name: string;
+  onClickForm: () => void;
+  onClickProfile: () => void;
+  onClickTrash: () => void;
 }
 
-export default function ClientProfile({ name }: ClientProfileProps) {
+export default function ClientProfile({ name, onClickForm, onClickProfile, onClickTrash }: ClientProfileProps) {
   return (
     <div className="bg-white w-full outline outline-1 outline-gray-400 h-14 rounded-2xl flex items-center px-6 justify-between">
           <p className="text-xl">{ name }</p>
           <div className="gap-4 flex">
-            <button><Image src={formIcon} alt="Form Icon"/></button>
-            <button><Image src={personIcon} alt="Person Icon"/></button>
-            <button><Image src={trashIcon} alt="Trash Icon"/></button>
+            <button onClick={onClickForm}><Image src={formIcon} alt="Form Icon"/></button>
+            <button onClick={onClickProfile}><Image src={personIcon} alt="Person Icon"/></button>
+            <button onClick={onClickTrash}><Image src={trashIcon} alt="Trash Icon"/></button>
           </div>
         </div>
   );
