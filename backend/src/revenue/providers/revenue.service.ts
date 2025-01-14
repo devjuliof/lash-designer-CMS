b2ff7f974f09ae455170886bfedc71d9f3252132
julio <juliofariadev@gmail.com>
2025-01-14 11:42:06 -0300

    feat: update revenue service with new methods for revenue calculation and data retrieval

--- backend/src/revenue/providers/revenue.service.ts
+++ backend/src/revenue/providers/revenue.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { AddRevenueDto } from './../dtos/revenue.dto';
-import { LessThan, MoreThan, Repository, TreeLevelColumn } from 'typeorm';
+import { MoreThan, Repository } from 'typeorm';
 import { Revenue } from '../entities/revenue.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 
