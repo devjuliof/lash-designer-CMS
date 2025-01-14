27e1c5cc94af9b9cb90d07826a2e1ebe37097672
julio <juliofariadev@gmail.com>
2025-01-14 11:30:26 -0300

    feat: create ClientInterface for client data structure

--- frontend/src/app/clients/interfaces/client.interface.ts
+++ frontend/src/app/clients/interfaces/client.interface.ts
@@ -1,4 +1,5 @@
 export interface ClientInterface {
+  map(arg0: (client: ClientInterface) => import("react").JSX.Element): import("react").ReactNode;
   id: number;
   name: string;
   aniversario: Date;
