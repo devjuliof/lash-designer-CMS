6a786adde7400d10236dce56071f72218fcf99fe
julio <juliofariadev@gmail.com>
2025-01-14 08:47:28 -0300

    feat: add debouncer to input search requests

--- frontend/src/app/clients/page.tsx
+++ frontend/src/app/clients/page.tsx
@@ -7,22 +7,43 @@ import PurpleButton from "./components/PurpleButton";
 import ClientProfile from "./components/ClientProfile";
 import { ClientsService } from "./services/clientsService";
 import { ClientInterface } from "./interfaces/client.interface";
+import PaginationFooter from "./components/PaginationFooter";
 
 export default function ClientsPage() {
   const [isFocused, setIsFocused] = React.useState(false);
   const [clients, setClients] = React.useState<ClientInterface | undefined>();
+  const [inputSearch, setInputSearch] = React.useState<string>('');
+
+  const debounceTimer = React.useRef<NodeJS.Timeout | null>(null);
 
   React.useEffect(() => {
-    async function fetchClients() {
-      const results = await ClientsService.getAllClients();
-      setClients(results.data);
+    (async() => {
+      if (!inputSearch) {
+        const results = await ClientsService.getAllClients();
+        setClients(results.data);
+      }
+    })()
+
+    if (debounceTimer.current) {
+      clearTimeout(debounceTimer.current);
     }
 
-    fetchClients();
-  }, []);
+    debounceTimer.current = setTimeout(async () => {
+      if (inputSearch) {
+        const results = await ClientsService.getSearchClientsByName(inputSearch);
+        setClients(results.data);
+      }
+    }, 300);
+
+    return () => {
+      if (debounceTimer.current) {
+        clearTimeout(debounceTimer.current);
+      }
+    };
+  }, [inputSearch]);
 
   return (
-    <div className="relative">
+    <div className="relative min-h-screen flex flex-col">
       <div
         className={`transition-opacity duration-500 ease-in-out ${
           isFocused ? 'opacity-0 pointer-events-none' : 'opacity-100'
@@ -30,21 +51,28 @@ export default function ClientsPage() {
       >
         <MenuMobile />
       </div>
-
+  
       <div
         className={`transition-transform duration-500 ease-in-out ${
           isFocused ? 'translate-y-[-2.5rem]' : ''
-        }`}
+        } flex-grow`}
       >
         <div className="flex gap-4 px-2 items-center">
-          <SearchInput isFocused={isFocused} setIsFocused={setIsFocused} />
+          <SearchInput isFocused={isFocused} setIsFocused={setIsFocused} setInputSearch={setInputSearch}/>
           <PurpleButton text={'+'} />
         </div>
-
+  
         <section className="px-2 mt-6 flex flex-col gap-4">
-          {clients && clients.map((client: ClientInterface) => <ClientProfile key={client.id} name={client.name}/>)}
+          {clients &&
+            clients.map((client: ClientInterface) => (
+              <ClientProfile key={client.id} name={client.name} />
+            ))}
         </section>
       </div>
+  
+      <div className="w-full px-2 flex justify-center mb-6">
+        <PaginationFooter />
+      </div>
     </div>
-  );
+  );  
 }
