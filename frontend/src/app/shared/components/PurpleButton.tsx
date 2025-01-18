7b378eb6a491b6db5f92e007354d9b8738921ae6
julio <juliofariadev@gmail.com>
2025-01-18 15:01:15 -0300

    feat: add disabled prop to PurpleButton component

--- frontend/src/app/shared/components/PurpleButton.tsx
+++ frontend/src/app/shared/components/PurpleButton.tsx
@@ -1,8 +1,18 @@
 type ButtonProps = {
+  disabled?: boolean;
   text: string;
   onClick: React.MouseEventHandler<HTMLButtonElement>;
-}
+};
 
-export default function PurpleButton({text, onClick}: ButtonProps) {
-  return <button className="bg-purple-500 text-white px-12 py-3 rounded-md font-bold" onClick={onClick}>{text}</button>
+export default function PurpleButton({ disabled, text, onClick }: ButtonProps) {
+  return (
+    <button
+      disabled={disabled}
+      className={`${disabled ? "bg-purple-200 cursor-not-allowed" : ""
+        } bg-purple-500 text-white px-12 py-3 rounded-md font-bold`}
+      onClick={onClick}
+    >
+      {text}
+    </button>
+  );
 }
