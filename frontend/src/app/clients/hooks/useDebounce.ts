019e5e7cfd26c0dcda6996c0af0f1d527f97c206
julio <juliofariadev@gmail.com>
2025-01-18 14:57:07 -0300

    feat: create useDebounce hook for managing debounced values

---
+++ frontend/src/app/clients/hooks/useDebounce.ts
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export function useDebounce(value: string, delay: number) {
+  const [debouncedValue, setDebouncedValue] = React.useState(value);
+
+  React.useEffect(() => {
+    if (!value) {
+      setDebouncedValue(value);
+      return;
+    }
+
+    const handler = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+}
