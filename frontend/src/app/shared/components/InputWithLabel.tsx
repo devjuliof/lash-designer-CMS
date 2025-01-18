19dd7468af257cd2d84985c4121dd4579ef080fc
julio <juliofariadev@gmail.com>
2025-01-18 14:59:53 -0300

    feat: add value prop support to InputWithLabel component

--- frontend/src/app/shared/components/InputWithLabel.tsx
+++ frontend/src/app/shared/components/InputWithLabel.tsx
@@ -3,16 +3,18 @@ import Input from "./Input";
 export default function InputWithLabel({
   label,
   placeholder,
+  value,
   onChange,
 }: {
   label: string;
-  placeholder: string;
+  placeholder?: string;
+  value?: string;
   onChange: React.ChangeEventHandler<HTMLInputElement>;
 }) {
   return (
     <div className="flex flex-col gap-1">
       <label className="text-gray-500 font-bold" htmlFor={label}>{label}</label>
-      <Input onChange={onChange} placeholder={placeholder}/>
+      <Input value={value} onChange={onChange} placeholder={placeholder} />
     </div>
   );
 }
