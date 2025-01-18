6f547352652288b376851a0c2ff8bbc4e0c546d9
julio <juliofariadev@gmail.com>
2025-01-18 14:58:47 -0300

    feat: add value prop to Input component

--- frontend/src/app/shared/components/Input.tsx
+++ frontend/src/app/shared/components/Input.tsx
@@ -1,8 +1,9 @@
 type InputProps = {
+  value?: string;
   onChange: React.ChangeEventHandler<HTMLInputElement>;
   placeholder?: string;
 }
 
-export default function Input({onChange, placeholder}: InputProps) {
-  return <input className="w-full h-12 px-2 rounded-xl border border-gray-400 outline-none" placeholder={placeholder} onChange={onChange}></input>
+export default function Input({ value, onChange, placeholder }: InputProps) {
+  return <input value={value} className="w-full h-12 px-2 rounded-xl border border-gray-400 outline-none" placeholder={placeholder} onChange={onChange}></input>
 }
