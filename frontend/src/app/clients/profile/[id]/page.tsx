71a8d380b65bd2c1b5269969ce76484bf1b28fa1
Julio Faria <juliofariadev@gmail.com>
2025-01-20 12:23:55 -0300

    feat(clients): implement profile page for individual clients

--- frontend/src/app/clients/profile/[id]/page.tsx
+++ frontend/src/app/clients/profile/[id]/page.tsx
@@ -1,32 +1,22 @@
 import { ClientsService } from "../../services/clientsService";
 import ClientForm from "../../components/ClientForm";
-import Image from "next/image";
 import backArrow from "../../assets/back-arrow.svg";
 import React from "react";
-import Link from "next/link";
-import { connection } from 'next/server'
+import Header from "../../components/Header";
 
 interface ProfilePageProps {
   params: Promise<{ id: string }>,
 }
 
 export default async function ProfilePage({ params }: ProfilePageProps) {
-  await connection();
   const { id } = await params;
   const clientId = Number(id);
-
 
   const client = await ClientsService.getClientById(clientId);
 
   return (
     <>
-      <div className="flex items-center justify-between p-2">
-        <Link href="/clients">
-          <Image src={backArrow} alt="back arrow" className="mr-2 cursor-pointer" />
-        </Link>
-        <h2 className="text-3xl text-center flex-grow">Cliente</h2>
-        <div className="w-[24px]"></div>
-      </div>
+      <Header linkHref={"/clients"} text={"Cliente"}/>
       <section className="p-6 flex flex-col gap-6">
         <ClientForm originalClient={client} />
       </section>
