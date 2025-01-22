23c08a04c11e70325c2d3ed5f821d5b51541d98d
Julio Faria <juliofariadev@gmail.com>
2025-01-22 18:32:22 -0300

    feat: create StringInput component using Input component

--- frontend/src/app/clients/components/StringInput.tsx
+++ frontend/src/app/clients/components/StringInput.tsx
@@ -3,13 +3,14 @@ import Input from "@/app/shared/components/Input";
 interface StringInputProps {
   text: string;
   onChange: React.ChangeEventHandler<HTMLInputElement>;
+  value: string | undefined;
 }
 
-export default function StringInput({ text, onChange }: StringInputProps) {
+export default function StringInput({ text, onChange, value }: StringInputProps) {
   return (
     <>
       <label htmlFor={text}>{text}</label>
-      <Input onChange={onChange} />
+      <Input value={value} onChange={onChange} />
     </>
   );
 }
