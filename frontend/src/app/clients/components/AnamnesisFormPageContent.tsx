a61855e87e99347c4577db8e9e708c5dfd5ea0f5
Julio Faria <juliofariadev@gmail.com>
2025-01-22 18:35:25 -0300

    feat: implement AnamnesisFormPageContent to handle different input types dynamically

--- frontend/src/app/clients/components/AnamnesisFormPageContent.tsx
+++ frontend/src/app/clients/components/AnamnesisFormPageContent.tsx
@@ -1,9 +1,8 @@
 'use client';
 
 import React from "react";
-import { AnamnesisForm } from "../interfaces/anamnesis-form.interface";
 import Header from "./Header";
-import SelectInput from "./SelectInput";
+import SelectInput, { IsChecked } from "./SelectInput";
 import StringInput from "./StringInput";
 import NumberInput from "./NumberInput";
 import { QuestionForm } from "../interfaces/question-form";
@@ -13,16 +12,75 @@ interface AnamnesisFormPageContentProps {
 }
 
 export default function AnamnesisFormPageContent({ questionsData }: AnamnesisFormPageContentProps) {
-  const [anamnesisForm, setAnamnesisForm] = React.useState(questionsData);
+  const [anamnesisForm, setAnamnesisForm] = React.useState<QuestionForm[]>(questionsData);
+
+  function handleOnChangeStringAndNumberInput(e, id) {
+    setAnamnesisForm((prevState) => 
+      prevState.map((questionForm) => {
+        if (questionForm.id === id) {
+          return {
+            ...questionForm,
+            value: e.target.value,
+          };
+        }
+        return questionForm;
+      })
+    );
+  }
+
+  function handleOnChangeSelectInput(value, id) {
+    setAnamnesisForm((prevState) => 
+      prevState.map((questionForm) => {
+        if (questionForm.id === id) {
+          return {
+            ...questionForm,
+            value: value,
+          };
+        }
+        return questionForm;
+      })
+    );
+  }
+  
 
   return (
     <>
       <Header linkHref={"/clients"} text={"Ficha de anamnese"} />
       {anamnesisForm.map((questionObject) => (
         <React.Fragment key={questionObject.id}>
-          { questionObject.type === 'string' && <StringInput text={questionObject.questionDescription} onChange={() => {}} />}
-          { questionObject.type === 'boolean' && <SelectInput text={questionObject.questionDescription} onChange={() => {}} />}
-          { questionObject.type === 'number' && <NumberInput text={questionObject.questionDescription} onChange={() => {}} />}
+          { questionObject.type === 'string' && (
+            <StringInput 
+              value={
+                typeof questionObject.value === 'string'
+                  ? questionObject.value : undefined
+              } 
+              text={questionObject.questionDescription} 
+              onChange={(value) => handleOnChangeStringAndNumberInput(value, questionObject.id)} 
+            />
+          )}
+          { questionObject.type === 'boolean' && (
+            <SelectInput 
+              text={questionObject.questionDescription}
+              value={
+                typeof questionObject.value === 'boolean'
+                  ? questionObject.value === true
+                    ? IsChecked.YES
+                    : IsChecked.NOT
+                  : undefined
+              } 
+              onChange={(e) => handleOnChangeSelectInput(e, questionObject.id)} 
+            />
+          )}
+          { questionObject.type === 'number' && (
+            <NumberInput 
+              value={
+                typeof questionObject.value === 'number'
+                  ? questionObject.value : undefined
+              } 
+              text={questionObject.questionDescription} 
+              onChange={(e) => handleOnChangeStringAndNumberInput(e, questionObject.id)} 
+            />
+          )}
         </React.Fragment>
       ))}
     </>
