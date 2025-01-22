ba5b0593fb3806531b8998be60b0b9b4c475d7b3
Julio Faria <juliofariadev@gmail.com>
2025-01-22 18:33:44 -0300

    feat: create SelectInput component with radio buttons for IsChecked enum

--- frontend/src/app/clients/components/SelectInput.tsx
+++ frontend/src/app/clients/components/SelectInput.tsx
@@ -1,16 +1,46 @@
-import { ChangeEventHandler } from "react";
+import React from "react";
 
-export default function SelectInput({ text, onChange } : { text: string, onChange: ChangeEventHandler<HTMLInputElement> }) {
+export enum IsChecked {
+  YES = 1,
+  NOT = 0,
+}
+
+interface SelectInputProps {
+  text: string;
+  onChange: (value: IsChecked) => void; 
+  value: IsChecked | undefined;
+}
+
+export default function SelectInput({ text, onChange, value }: SelectInputProps) {
+  const [isChecked, setIsChecked] = React.useState<IsChecked | undefined>(value);
+
+  const handleChange = (newValue: IsChecked) => {
+    setIsChecked(newValue);
+    onChange(newValue);
+  };
+
   return (
     <>
       <label htmlFor={text}>{text}</label>
       <div className="flex gap-4">
         <div>
-          <input name={text} id={`${text}-id-1`} type="radio"></input>
+          <input
+            checked={isChecked === IsChecked.YES}
+            name={text}
+            id={`${text}-id-1`}
+            type="radio"
+            onChange={() => handleChange(IsChecked.YES)}
+          />
           <label htmlFor={`${text}-id-1`}>Sim</label>
         </div>
         <div>
-          <input name={text} id={`${text}-id-2`} type="radio"></input>
+          <input
+            checked={isChecked === IsChecked.NOT}
+            name={text}
+            id={`${text}-id-2`}
+            type="radio"
+            onChange={() => handleChange(IsChecked.NOT)}
+          />
           <label htmlFor={`${text}-id-2`}>Não</label>
         </div>
       </div>
