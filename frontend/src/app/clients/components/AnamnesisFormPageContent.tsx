28394c27efdacb0f535071847c1eeab8144474ef
Julio Faria <juliofariadev@gmail.com>
2025-01-22 14:44:28 -0300

    feat: render dynamic question inputs based on form data

--- frontend/src/app/clients/components/AnamnesisFormPageContent.tsx
+++ frontend/src/app/clients/components/AnamnesisFormPageContent.tsx
@@ -3,17 +3,28 @@
 import React from "react";
 import { AnamnesisForm } from "../interfaces/anamnesis-form.interface";
 import Header from "./Header";
+import SelectInput from "./SelectInput";
+import StringInput from "./StringInput";
+import NumberInput from "./NumberInput";
+import { QuestionForm } from "../interfaces/question-form";
 
 interface AnamnesisFormPageContentProps {
-  formData: AnamnesisForm,
+  questionsData: QuestionForm[],
 }
 
-export default function AnamnesisFormPageContent({ formData }: AnamnesisFormPageContentProps) {
-  const [anamnesisForm, setAnamnesisForm] = React.useState<AnamnesisForm>(formData);
+export default function AnamnesisFormPageContent({ questionsData }: AnamnesisFormPageContentProps) {
+  const [anamnesisForm, setAnamnesisForm] = React.useState(questionsData);
 
   return (
     <>
       <Header linkHref={"/clients"} text={"Ficha de anamnese"} />
+      {anamnesisForm.map((questionObject) => (
+        <React.Fragment key={questionObject.id}>
+          { questionObject.type === 'string' && <StringInput text={questionObject.questionDescription} onChange={() => {}} />}
+          { questionObject.type === 'boolean' && <SelectInput text={questionObject.questionDescription} onChange={() => {}} />}
+          { questionObject.type === 'number' && <NumberInput text={questionObject.questionDescription} onChange={() => {}} />}
+        </React.Fragment>
+      ))}
     </>
   );
 }
